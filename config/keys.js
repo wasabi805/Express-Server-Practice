a9a83c07ab5d5cd0a4207b1debf6baab01e0cb01
‘Tim <timothy.j.ocampo@gmail.com>
2018-04-30 12:09:19 -0700

    TITLE : Added config dir with keys.js && connect mongoose to db
    	-created ./config/keys.js
    		this file has obj with key of mongoURI with value of string for sanbox mongoDB hosted on mlab.com
    
    	-in server.js:
    		- required mongoose
    		- defined const db (which is just the object from ./config/keys that points to my sandbox mlab db )
    	- made mongoose connection to db on lines 15-18 in server.js

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+//DEVELOPMENT key for sandbox db on https://mlab.com
+
+
+module.export={
+    mongoURI: "mongodb://<wasabi805>:<wasabi805>@ds163689.mlab.com:63689/express-practice"
+};
--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app=express();
 
-app.get('/', (req,res)=> res.send('Hello'));
+//DB Config
+const db = require('./config/keys').mongoURI; // .mongoURI is key from ./config/keys
+
+
+//Connect to MongoDB
+
+// mongoose.connect(db).then(()=>console.log('mongoDb Connected')).catch(err=>console.log(err));
+// below me is the line above me split to read easier...
+
+mongoose
+    .connect(db)//connect mongoose to our database
+    .then(()=>console.log('mongoDb Connected'))//if connection is SUCCESS
+    .catch(err=>console.log(err));// return error if connect fails
+
+
+app.get('/', (req,res)=> res.send('Hello !!!'));
 
 const port = process.env.port || 5000; // Note: process.env.port used for Hiroku deploy. later on
 
