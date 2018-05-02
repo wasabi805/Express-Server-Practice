3fdf8a1cbeb96439d97f5c1feaed938992d85cfd
‘Tim <timothy.j.ocampo@gmail.com>
2018-05-02 11:35:31 -0700

    TITLE: PASSPORTJWT AUTH: Validate Login credentials
    	-IMPORTANT NOTE: To avoid any unauthorized errors,
    		remember for succesful a login within the login route logic inside /routes/api/users.js,
    		the token key returns:
    
    		'Bearer ' + token	<== white space included instring
    				NOT....
    		'Bearer' + token	<== no white space
    
    	///////////////////////////////////////////////////////////////////////////////////////////////
    
    	- need passport brought in (server.js ln 4)
    	- inititial passport (server.js ln 30)
    	- enable passport to be exported to other files in project (server.js ln 33)
    
    		- created config file for passport (see config/passport.js)
    		- set up the file configs : read the entire file and all comments
    		- ln 14, ==> Bearer is simply "an HTTP authentication scheme"
    		- ln 25 && ln 28  ==> passport was imported from server.js ln 33
    		- ln 29 jwt_payload is an arg :this arg contains the data inside const newUser, from user.js ln 39
    
    			- jwt_payload obj is used to to target user data returned from db which I'll  use later to render
    			database stuff
    
    			-'done' has 2  args: 1st arg = errors, 2nd arg =" user" or" not the user"
    
    	- config/keys.js should contain a key val pair for secret: My key is secretOrKey. The value is string of 'secret'
    
    	- routes/api/users.js
    		- brought in jwt, keys, and passport (see ln 7-9)
    		- Main addition to this file is the login and current routess ( starts from ln 70 ends at the end of the file)
    		- current is a get route that retrieves the data of current logged-in user only after they've been authenticated with the jwt-passport token.
    			- Of course, authentication happenes after user submits email and password they registered with

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,9 +6,11 @@ const gravatar =require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
+const passport = require('passport');
 
 //Load the User Model
 const User = require('../../models/User');
+
 
 
 //@ROUTES   GET api/users/test
@@ -16,14 +18,12 @@ const User = require('../../models/User');
 //@access   PUBLIC
 
 router.get('/test', (req,res)=>{
-    res.json({msg: "Users WORKS!!"}) //.json() puts our res from cb into JSON format
+    res.json({msg: "Users WORKS!!"})
 } );
-
 
 //@ROUTES   GET api/users/register
 //@desc     Registers a user
 //@access   PUBLIC
-
 
 router.post('/register', (req,res)=>{
 
@@ -33,8 +33,8 @@ router.post('/register', (req,res)=>{
                 return res.status(400).json({email: 'Email already exists'})
             }
             else{
-                // see : https://github.com/emerleite/node-gravatar  ==> Usage
-                const avatar = gravatar.url(req.body.email, {s:'200', r: 'pg', d: 'mm'});  //s=size, r=rating, d=default : mm= blank avatar
+
+                const avatar = gravatar.url(req.body.email, {s:'200', r: 'pg', d: 'mm'});
 
                 const newUser = new User({
                     name: req.body.name,
@@ -46,12 +46,17 @@ router.post('/register', (req,res)=>{
 
                 bcrypt.genSalt(10, (err, salt)=>{
                     bcrypt.hash(newUser.password, salt, (err, hash)=>{
-                        if(err) throw err;
-                        newUser.password= hash;
-                        newUser
-                            .save()
-                            .then(user=>res.json(user))
-                            .catch(err=> console.log(err));
+                        if(err){
+                            console.log('There was an error->(users.js ln49)');
+                            throw err;
+                        }
+                        else{
+                            newUser.password= hash;
+                            newUser
+                                .save()
+                                .then(user=>res.json(user))
+                                .catch(err=> console.log(err));
+                        }
                     })
                 })
             }
@@ -74,43 +79,57 @@ router.post('/login', (req, res)=>{
                 return res.status(400).json({email:'user not found'})
             }
 
-            bcrypt.compare(password, user.password)//compare password(const from ln 65 vs. pw from db=> user.password): .compare evaluates to true or false
-                .then(isMatch=>{
+            //check Password
+            bcrypt.compare(password, user.password).then(isMatch=>{
+
                     if(isMatch){
-                        // res.json({msg: 'Success'});
-
-                        //create the payload to pass into jwt.sign()
+                        //User Match
                         const payload={
-                            id: user.id,
-                            name: user.name,
-                            avatar: user.avatar
+                            id: user.id, name: user.name, avatar: user.avatar
                         };
-
-                        //Sign Token / create the token
-                        // 1st arg is from above,
-                        // 2nd arg is the key ==> key placed in './config/keys.js', see ln 8 of this file.
-                        // 3rd arg is the time key expires
-                        // 4th arg is cb
+                        //Sign in Token
                         jwt.sign(
                             payload,
                             keys.secretOrKey,
-                            {expiresIn: 3600},//3600 = 1 hour
+                            { expiresIn: 7200 },
                             (err, token)=>{
                                 res.json({
                                     success: true,
-                                    token: 'Bearer' + token //Bearer is a type of token with it's own protocol
-                                    //NOTE: on success, token will go into the header as auth.(Will cover later)
+                                    token: 'Bearer ' + token
                                 });
-                        });
+                            }
+                        );
                     }
                     else{
                         return res.status(400).json({password: 'Password incorrect'})
                     }
-                })
-
+                });
         });
 
 });
+
+//@ROUTES   GET api/users/current
+//@desc     Return the current/ logged in user
+//@access   PRIVATE
+
+//1st arg = name of route
+//2nd arg = authentication method which takes args 'jwt == ln 8 of this file && no session used
+//3rd arg = cb of res && req
+
+
+
+router.get('/current', passport.authenticate('jwt', {session:false}),
+    (req,res)=>{
+    // res.json({msg: 'success from users.js, ln 121'});
+    //     res.json(req.user) //remember that user is already in the req arg : see config/passport.js ==> .then(user)
+
+        res.json({
+            id: req.user.id,
+            name: req.user.name,
+            email: req.user.email
+        })
+    }
+);
 
 //export the router
 module.exports=router;
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser=require('body-parser'); //see './models/User.js' line 30
-
-const app=express();
+const passport = require('passport');
 
 const users=require('./routes/api/users');
 const profile=require('./routes/api/profile');
 const posts=require('./routes/api/posts');
 
+const app=express();
 
 //Body parser middleware
 app.use(bodyParser.urlencoded({extended: false})); //see './models/User.js' line 29
@@ -26,8 +26,12 @@ mongoose
     .then(()=>console.log('mongoDb Connected'))
     .catch(err=>console.log(err));
 
+//Passport (Middleware)
+app.use(passport.initialize());
 
-app.get('/', (req,res)=> res.send('Hello !!!'));
+//Passport Config
+require('./config/passport')(passport);
+
 
 
 //Use Routes
@@ -39,3 +43,5 @@ app.use('/api/posts', posts);
 const port = process.env.port || 5000;
 
 app.listen(port, ()=>console.log(`Server running on port ${port}`));
+
+
