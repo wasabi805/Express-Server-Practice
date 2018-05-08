f3140515a281952f51f7caface0871cbfee2ac06
‘Tim <timothy.j.ocampo@gmail.com>
2018-05-08 14:50:19 -0700

    TITLE: Add and remove Comments route
    	- see all changes for routes/api/posts.js

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -162,8 +162,75 @@ router.post('/unlike/:id', passport.authenticate('jwt', {session: false}),
         });
     });
 
+//@ROUTES   POST api/posts/comment/:id  ==> NOTE: the id belongs to the post so we know which post will receive the comment
+//@desc     Add a COMMENT on another users' post
+//@access   Private
 
 
+router.post('/comment/:id', passport.authenticate('jwt', {session: false}),
+    (req,res)=>{
 
+        const {errors, isValid} = validatePostInput(req.body);
+
+        if(!isValid){
+            return res.status(400).json(errors);
+        }
+
+        Post.findById(req.params.id)
+        //after we get back the post id, create a new comment(see below)
+            .then(post=>{
+
+                const newComment ={
+                    text: req.body.text,
+                    name: req.body.name,
+                    avatar: req.body.avatar,
+                    user: req.user.id
+                };
+
+                //Add newComment to the comments array
+
+                post.comments.unshift(newComment);
+
+                post.save().then(post=> res.json(post))
+
+            })
+            .catch(err=>res.status(404).json({postnotfound: 'No post found.(from catch)'}))
+
+    });
+
+//@ROUTES   DELETE api/posts/comment/:id/:comment_id  ==> NOTE: the :id belongs to the post, :comment_id belongs to which comment of that post id
+//@desc     REMOVE a specific COMMENT on another users' specific post
+//@access   Private
+
+
+router.delete('/comment/:id/:comment_id', passport.authenticate('jwt', {session: false}),
+    (req,res)=>{
+
+        Post.findById(req.params.id)
+        //after we get back the post id, create a new comment(see below)
+            .then(post=>{
+
+               //Check to see if the comment exists:
+                // - comment/:id/:comment_id  === req.params.comment_id
+                // if this is true, then the comment doesn't exist in the comments array
+                if(post.comments.filter(comment=>comment._id.toString() === req.params.comment_id).length===0){
+                    return res.status(404).json({commentnotexist: 'Comment does not exist.'});
+                }
+
+                //if the comment does exist.. map the comments array to remove the comment.
+
+                //Get remove index
+                const removeIndex = post.comments.map(item=>item._id.toString()).indexOf(req.params.comment_id);
+
+                //Splice out of the array
+                post.comments.splice(removeIndex,1);
+
+                //Save the post
+                post.save().then(post=>res.json(post));
+
+            })
+            .catch(err=>res.status(404).json({postnotfound: 'No post found.(from catch)'}))
+
+    });
 
 module.exports=router;
