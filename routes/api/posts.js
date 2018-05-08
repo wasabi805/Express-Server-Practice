9447aa4812fc479cb1a8b3ba32e0ee27d31b689a
‘Tim <timothy.j.ocampo@gmail.com>
2018-05-08 13:14:55 -0700

    TITLE: Get and Delete Post routes
    	- see all changes for routes/api/posts.js

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -8,6 +8,10 @@ const passport = require('passport');
 //Post model/Schema
 const Post = require('../../models/Post');
 
+//Profile model
+
+const Profile = require('../../models/Profile');
+
 //Validation
 const validatePostInput = require('../../validation/post');
 
@@ -20,8 +24,33 @@ router.get('/test', (req,res)=>{
     res.json({msg: "POSTS WORKS!!"})
 } );
 
+//@ROUTES   GET api/posts
+//@desc     fetches ALL posts
+//@access   PUBLIC
 
-//create a route that handles posts
+router.get('/', (req,res)=>{
+
+    Post.find()
+        .sort({ date: -1 })
+        .then(posts=>res.json(posts))
+        .catch(err=>res.status(404).json({nopostsfound: 'No posts found.'}))
+} );
+
+
+//@ROUTES   GET api/posts/:id
+//@desc     fetches A SINGLE posts
+//@access   PUBLIC
+
+router.get('/:id', (req,res)=>{
+
+    Post.findById(req.params.id)
+        .sort({ date: -1 })
+        .then(post=>res.json(post))
+        .catch(err=>res.status(404).json({nopostfound: 'No post found with that id.'}))
+} );
+
+
+//Create a route that handles posts
 
 //@ROUTES   POST api/posts
 //@desc     create post
@@ -46,6 +75,33 @@ router.post('/', passport.authenticate('jwt', {session: false}), (req,res)=>{
     newPost.save().then(post=>res.json(post));
 
 } );
+
+
+//@ROUTES   DELETE api/posts/:id
+//@desc     delete a single post
+//@access   Private
+
+router.delete('/:id', passport.authenticate('jwt', {session: false}),
+    (req,res)=>{
+
+        Profile.findOne({user: req.user.id}).then(profile=>{
+            Post.findById(req.params.id).then(post=>{
+                //check for the post owner
+                //the if stmnt checks against req.user.id
+                //keep in mind, post.user !=String vs. req.user.id == String
+                if(post.user.toString() !== req.user.id){
+                    return  res.status(401).json({notauthorized: 'User not authorized'})
+                }
+
+                //..if it is the author of the post...
+
+                post.remove().then(()=>res.json({success: true}))
+                    .catch(err=>res.status(404).json({postnotfound: 'Post not found.'}))
+
+            })
+        })
+    });
+
 
 
 
