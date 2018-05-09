d99a53d6222719ec3f986279f0a58992a0152b98
‘Tim <timothy.j.ocampo@gmail.com>
2018-05-08 17:47:07 -0700

    TITLE: React Router : Basic Set up
    	- installed react-router: ALWAYS install into React stuff directory
    		IMPORTANT: when installing, make sure that you cd into client dir to install
    		in terminal, from root dir ==> cd into client ==> npm i react-router.dom
    
    	- brought in BrowserRouter and renamed as Router (see client/src/App.js ln 3)
    		- this mimics a standard server
    		- brought in Route (see client/src/App.js ln 3)
    
    	- wrapped main div from App.js with < Router> (see client/src/App.js ln 15 && ln 27)
    
    	- created route for landing comp (see client/src/App.js ln 18)
    
    	- creted auth dir for reg and login comps
    
    	- imported { Link } into Navbar, Login, and Registration comps
    		- changed a tags to Link
    		- changed href="" =into=>   to="/[routeGoesHere]" inside the Navbar comp
    		- removed .html suffix within to=""
    
    		- recap, <a href="/somePathToLink"> =turns into=> <Link to="/myPathToLink"> objectToClick  </link

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Component} from 'react';
+
+class Register extends Component{
+    render(){
+        return(
+            <div>
+                <h1>TEST FOR REG ROUTE</h1>
+            </div>
+        )
+    }
+}
+
+export default Register
