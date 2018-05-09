158e9897d037a95bbde6f0566bc530beb5712958
‘Tim <timothy.j.ocampo@gmail.com>
2018-05-09 14:30:39 -0700

    TITLE: Redux Store and Chomre Ext : Set up Provider,Store,reducers,
    	- see all comments in the file for what changes were made.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { Provider } from 'react-redux'; //Provider provides the store and needs to wrap around everything
+import store from './store'; // imported store from client/store.js
 
 import './App.css';
 import Navbar from "./components/layout/Navbar";
@@ -9,22 +11,40 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
+// ===========  FOR REDUX   ===========
+//wrap EVERYTHING in the provider (ln 33 && 47)
+
+//          Create the store
+
+// To make the store, we need below..
+// const store = createStore(()=> [], {}, applyMiddleware());
+
+//  1st param : the root reducer ==>[]
+//  2nd param : the initial state ==> {}
+//  3rd param : the middleware ===> applyMiddleware());
+//  see Arguments @  https://github.com/reactjs/redux/blob/master/docs/api/createStore.md  for more details
+
+// For configuring store, we don't want it in this file so we'll give it it's own file for breathing room and import it
+//back into App.js
+
 class App extends Component {
   render() {
     return (
-        <Router>
-          <div className="App">
-              <Navbar/>
-                <Route exact path="/" component={ Landing }/>
-              <Footer/>
+        <Provider store={store}>
+            <Router>
+              <div className="App">
+                  <Navbar/>
+                    <Route exact path="/" component={ Landing }/>
+                  <Footer/>
 
-              <div className="container">
-                  <Route exact path='/register' component={ Register }/>
-                  <Route exact path='/login' component={ Login }/>
+                  <div className="container">
+                      <Route exact path='/register' component={ Register }/>
+                      <Route exact path='/login' component={ Login }/>
+                  </div>
+
               </div>
-
-          </div>
-        </Router>
+            </Router>
+        </Provider>
     );
   }
 }
