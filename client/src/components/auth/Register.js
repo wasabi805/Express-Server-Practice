411ba1bb855080780422841624312eaf2e408c12
‘Tim <timothy.j.ocampo@gmail.com>
2018-05-09 13:21:21 -0700

    TITLE: Error handling && display on form (red boxes) : installed classNames
    	- installed classnames:
    		- cd into client dir ==then==>  npm i classnames
    		- this is used to turn the form box red on error
    		- imported into Register.js
    
    	- refactored classNames attr in form to include classnames()
    		- 1st params are the default classnames used for format and style
    		- 2nd param: 'is-invalid' is the key name needed for classnames() ability to turn form box red (See Docs for classnames)
    			- the value for 'is-invalid' key is errors.name from the errors obj inside state
    			- ln 60 of this file implement deconstruction so we can call the errors obj from state.
    
    		- keep in mind that the actual error generated originates from our back end validation (ie, validation/register.js)
    			- remember that the server then responds back with errors (if any) and,
    			 that those errors are stored in the data arg of validateRegisetrInput from validation/register.js (ln 8)
    
    	- see ln 90 of this file:
    		ex.) {errors.name && (<div className="invalid-feedback">{errors.name} </div>)}
    			- place this below the closing tag for input but, STILL inside the div: will not render if outside the div.
    
    	- in case this wasn't mentioned from the last push, see ln 44-52:
    		- reformatted the catch to change state for errors to include them in the states errors obj if there are any.
    		- initially, this catch was only set up to console.log for testing/ learning purpose
    
    	-read all the inline comments for further clarification

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Component} from 'react';
 
 import axios from 'axios';
-
+import classnames from 'classnames';
 
 class Register extends Component{
 
@@ -43,12 +43,23 @@ class Register extends Component{
         //1st param is the route to register in express server (see routes/api/users.js ln 23)
         axios.post('/api/users/register',newUser)
             .then(res=> console.log(res.data))
-            .catch(err=>console.log(
-                err.response.data, 'from catch, Register.js axios,/register '))
-                // err.response.data gives us the error msg's obj that I created earlier from setting up the server
+            .catch(err=> this.setState({
+                errors : err.response.data
+            }))
+
+        // For reference ==used to be..=> .catch(err=>console.log(err.response.data))
+        // move the errors from error obj{} created in backend to change init state of errors{} from state inside constructor.
+        // if there is an error, the form box will turn red
     }
 
     render(){
+
+        //the line below is the same thing as :
+        // const errors = this.state.errors ==> think of it as, "you can pull errors out of this.state."
+
+        const { errors } = this.state;
+
+
         return(
             <div className="register">
                 <div className="container">
@@ -57,54 +68,73 @@ class Register extends Component{
                             <h1 className="display-4 text-center">Sign Up</h1>
                             <p className="lead text-center">Create your DevConnector account</p>
 
-
-
-                            <form onSubmit={this.onSubmit}>
+                            {/*added noValidate as an attr to remove default HTML5 validations*/}
+                            <form noValidate onSubmit={this.onSubmit}>
 
                                 <div className="form-group">
 
                                     <input type="text"
-                                           className="form-control form-control-lg"
+                                           // className="form-control form-control-lg" //==> for reference of what className used to be..
+                                           className={classnames('form-control form-control-lg',
+                                               {'is-invalid': errors.name}
+                                               )}
+                                           //Note for ln above: 'is-invalid' will only appear if errors.name exists / if there is is a name, then the name error will not exist.
+                                            // Remember that errors.name originates from validation/register from the backend (ln 22-24 or 28-30) depending on which error
                                            placeholder="Name"
                                            name="name"
                                            value={this.state.name}
                                            onChange={this.onChange}
                                     />
+
+                                    {/*insert the error for the user to view on client side*/}
+                                    {errors.name && (<div className="invalid-feedback">{errors.name} </div>)}
+
                                 </div>
 
                                 <div className="form-group">
 
                                     <input type="email"
-                                           className="form-control form-control-lg"
+                                           className={classnames('form-control form-control-lg',
+                                               {'is-invalid': errors.email}
+                                           )}
                                            placeholder="Email Address"
                                            name="email"
                                            value={this.state.email}
                                            onChange={this.onChange}
 
                                     />
+                                    {errors.email && (<div className="invalid-feedback">{errors.email} </div>)}
 
                                     <small className="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
                                 </div>
 
                                 <div className="form-group">
                                     <input type="password"
-                                           className="form-control form-control-lg"
+                                           className={classnames('form-control form-control-lg',
+                                               {'is-invalid': errors.password}
+                                           )}
                                            placeholder="Password"
                                            name="password"
                                            value={this.state.password}
                                            onChange={this.onChange}
                                     />
+                                    {errors.password && (<div className="invalid-feedback">{errors.password} </div>)}
                                 </div>
+
 
                                 <div className="form-group">
                                     <input type="password"
-                                           className="form-control form-control-lg"
+                                           className={classnames('form-control form-control-lg',
+                                               {'is-invalid': errors.password2}
+                                           )}
                                            placeholder= "Confirm Password"
                                            name="password2"
                                            value={this.state.password2}
                                            onChange={this.onChange}
                                     />
+                                    {errors.password2 && (<div className="invalid-feedback">{errors.password2} </div>)}
                                 </div>
+
 
                                 <input type="submit"
                                        className="btn btn-info btn-block mt-4"
