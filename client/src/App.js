267abee1e921b270a1b41b8635ca564050113c96
‘Tim <timothy.j.ocampo@gmail.com>
2018-05-15 16:33:22 -0700

    TITLE: ADD experience for
    	- see all changes and <AddExperience/>

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,8 @@ import Login from "./components/auth/Login";
 import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from './components/create-profile/CreateProfile';
 import EditProfile from './components/edit-profile/EditProfile';
+import AddExperience from './components/add-credentials/AddExperience';
+
 
 import setAuthToken from './utils/setAuthToken';
 import jwt_decode from 'jwt-decode';
@@ -71,6 +73,10 @@ class App extends Component {
 
                       <Switch>
                           <PrivateRoute exact path='/edit-profile' component={ EditProfile }/>
+                      </Switch>
+
+                      <Switch>
+                          <PrivateRoute exact path='/add-experience' component={ AddExperience }/>
                       </Switch>
                   </div>
                   <Footer/>
---
+++ client/src/components/add-credentials/AddEducation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Component} from 'react';
+
+class AddEducation extends Component{
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default AddEducation
