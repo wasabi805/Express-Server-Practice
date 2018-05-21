7009b61941fc91f1d9488d88a53906fd16dd5fe0
‘Tim <timothy.j.ocampo@gmail.com>
2018-05-21 12:00:16 -0700

    TITLE : Profile Items
    	- built class comp for profile items
    	- imported and can now render all profiles

---
+++ client/src/components/profiles/ProfileItem.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+import {Link } from 'react-router-dom';
+import isEmpty from '../../validation/is-empty';
+import PropTypes from 'prop-types'
+
+
+class ProfileItem extends Component{
+    render(){
+
+        const {profile} = this.props;
+
+        return(
+            <div className='card card-body bg-light mb-3'>
+                <div className='row'>
+                    {/*NOTE: remember on the back end, we used populate: allows us to extract user name for avatar from 'user'*/}
+                    <div className='col-2'>
+                        <img src={profile.user.avatar} className='rounded-circle'/>
+                    </div>
+
+                    <div className='col-lg-6 col-md-4 col-8'>
+                        <h3>{profile.user.name}</h3>
+                        {/*if they have a company, display company along with status*/}
+                        <p>
+                            {profile.status} {isEmpty(profile.company) ? null: (<span>at {profile.company}</span>)}
+                        </p>
+
+                        <p>
+                            {isEmpty(profile.location) ? null : (<span>{profile.location}</span>)}
+                        </p>
+
+                        <Link to={`/profile/${profile.handle}`} className='btn btn-info'>
+                            View Profile
+                        </Link>
+
+                    </div>
+                    {/*if below a medium size, display none, if above medium size screen display as block */}
+                    <div className='col-md-4 d-none d-md-block'>
+                        <h4>Skill Set</h4>
+                        <ul className='list-group'>
+
+                            {/*map through the skills [], map the first 4 */}
+                            {profile.skills.slice(0,4).map(
+                                (skill, index)=>
+                                    (<li key={index} className='list-group-item'>
+                                        <i className='fa fa-check pr-1'/>
+                                        {skill}
+                                    </li>)
+                            )}
+                        </ul>
+                    </div>
+
+
+
+                </div>
+
+            </div>
+        )
+    }
+}
+
+
+
+ProfileItem.propTypes={
+    profile: PropTypes.object.isRequired
+};
+
+export default ProfileItem
+
--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Spinner from '../common/Spinner';
 import { getProfiles } from '../../actions/profileActions';
+import ProfileItem from './ProfileItem';
 
 class Profiles extends Component {
 
@@ -19,7 +20,10 @@ class Profiles extends Component {
             profileItems = <Spinner />;
         } else {
             if (profiles.length > 0) {
-                profileItems = <div>TODOItem : PROFILES WILL GO HERE</div>
+                profileItems = profiles.map(profile=>(
+
+                    <ProfileItem key={profile._id} profile={profile}/>
+                ))
 
             } else {
                 profileItems = <h4>No profiles found...</h4>;
