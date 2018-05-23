b839817c7e1eb0c3a4fc8ba3b1a214d10adcc587
‘Tim <timothy.j.ocampo@gmail.com>
2018-05-23 16:57:09 -0700

    TITLE : Attemp at fixing problems from switching from testArea to master branch
    	- in cmd ln, ran git:
    		 rm --cached .idea/workspace.xml

---
+++ config/keys_dev.js
@@ -0,0 +1,7 @@
+//DEVELOPMENT key for sandbox db on https://mlab.com
+
+//will need to bring in secretOrKey into the './routes/api/users.js' (see users.js ln 8)
+module.exports={
+    mongoURI: 'mongodb://wasabi805:wasabi805@ds163689.mlab.com:63689/express-practice',
+    secretOrKey: 'secret'
+};
