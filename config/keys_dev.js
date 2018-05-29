ac5bf761d0e985e00629f09b7278d68a38bb96cf
‘Tim <timothy.j.ocampo@gmail.com>
2018-05-29 13:33:28 -0700

    test deploy3

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,6 +1,3 @@
-//DEVELOPMENT key for sandbox db on https://mlab.com
-
-//will need to bring in secretOrKey into the './routes/api/users.js' (see users.js ln 8)
 module.exports={
     mongoURI: 'mongodb://wasabi805:wasabi805@ds163689.mlab.com:63689/express-practice',
     secretOrKey: 'secret'
--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,3 @@
-
-//The values can only be interpreted by the server for the App's security.
 module.exports={
     mongoURI: process.env.MONGO_URI,
     secretOrKey: process.env.SECRET_OR_KEY
