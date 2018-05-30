7af3301ef7ea32f540e3a3e1abb35d4e32251b25
‘Tim <timothy.j.ocampo@gmail.com>
2018-05-29 21:24:55 -0700

    cleaned up store.js

--- client/src/store.js
+++ client/src/store.js
@@ -1,20 +1,18 @@
-import {createStore, applyMiddleware, compose} from 'redux'; // import from redux
-import thunk from 'redux-thunk'; //import thunk
-import rootReducer from './reducers'; //import the root reducer, then pass into createStore as 1st param
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 
-const middleware = [thunk]; // made const to pass into applyMiddleware()
-const initialState = {}; // define so I can pass into createStore
+const initialState = {};
 
-
+const middleware = [thunk];
 
 const store = createStore(
     rootReducer,
     initialState,
     compose(
         applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // this lengthy line is used to implement the Redux dev tool ext.
-        )); // used spread operator for middleware
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+);
 
-
-//send the store back to App.js so we can access it there
 export default store;
