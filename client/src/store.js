d4cbe03c7499ebb772103dc1e6b8b7952db713db
‘Tim <timothy.j.ocampo@gmail.com>
2018-05-30 02:21:26 -0700

    edit store.js with 1.2 advanced store setup

--- client/src/store.js
+++ client/src/store.js
@@ -8,14 +8,35 @@ const middleware = [thunk];
 
 
 
+// const store = createStore(
+//     rootReducer,
+//     initialState,
+//     compose(
+//         applyMiddleware(...middleware),
+//         window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+//     )
+// );
+
+
+const composeEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : compose;
+
+
+const enhancer = composeEnhancers(
+    applyMiddleware(...middleware)
+    // other store enhancers if any
+);
+
+
 const store = createStore(
     rootReducer,
+    enhancer,
     initialState,
-    compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-    )
 );
+
+
+
 //
 export default store;
 
