55a124927ba7534b7c6d0c5095779e49eb4714fa
‘Tim <timothy.j.ocampo@gmail.com>
2018-05-29 22:42:57 -0700

    store.js

--- client/src/store.js
+++ client/src/store.js
@@ -11,9 +11,8 @@ const store = createStore(
     initialState,
     compose(
         applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
 );
 
-//
 export default store;
